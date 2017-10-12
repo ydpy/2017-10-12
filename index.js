771bad54477df1feb7738d723928fafa2d4be211
ydpy <1078399374@qq.com>
2017-10-12 16:56:48 +0800

    down

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 var box = document.querySelector('.box');
+box.addEventListener('mousedown',down);
+function down(ev){
+    var starX = ev.clientX - box.offsetLeft;
+    var starY = ev.clientY - box.offsetTop;
+}

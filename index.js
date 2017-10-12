fd4cdebb80e6236f559c118b0b3e695b44edbf00
ydpy <1078399374@qq.com>
2017-10-12 17:48:52 +0800

    down move and up

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-            
+
 var box = document.querySelector('.box');
 box.addEventListener('mousedown',down);
 function down(ev){
     var starX = ev.clientX - box.offsetLeft;
     var starY = ev.clientY - box.offsetTop;
-
     document.addEventListener('mousemove',move);
     function move(ev){
-        box.style.marginLeft = ev.clientX - starX;
-        box.style.marginTop = ev.client - starY;
+        box.style.left = ev.clientX - starX + "px";
+        box.style.top = ev.clientY - starY + "px";
     }
     document.addEventListener('mouseup',up);
     function up(ev){
-        document.removeEventListener('mousedown',down);
+        document.removeEventListener('mousemove',move);
         document.removeEventListener('mouseup',up);
     }
 }
